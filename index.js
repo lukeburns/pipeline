9e686c1d408eae3d1ce3b19d3bf214eb1b068342
Luke Burns <luke@lukeburns.com>
2014-07-16 12:25:22 -0400

    separate prepare from pipeline

--- index.js
+++ index.js
@@ -5,17 +5,21 @@ var Stream = require('stream'),
 
 module.exports = pipeline;
 
-function pipeline() {
-	var streams = Array.prototype.concat.apply([], arguments).filter(function (stream) {
-    return Boolean(stream) && Boolean(stream.pipe);
-  }).map(function(stream) {
-  	return !(stream instanceof Stream.Readable || stream.readable) ? pass(stream) : stream;
-  });
+function pipeline () {
+	var streams = pipeline.prepare.apply(this, arguments);
 
   if(streams.length == 0) return through();
   if(streams.length > 1) streams.push(repipe(streams[0], streams[streams.length - 1]));
 
 	return streams.reduce(function (previous, current) {
     return previous.pipe(current);
+  });
+}
+
+pipeline.prepare = function () {
+  return Array.prototype.concat.apply([], arguments).filter(function (stream) {
+    return Boolean(stream) && Boolean(stream.pipe);
+  }).map(function(stream) {
+    return !(stream instanceof Stream.Readable || stream.readable) ? pass(stream) : stream;
   });
 }
